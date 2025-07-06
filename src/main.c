0e696a26caa3d5227f44f9aff655b6040f2dd80a
kobayashi <a9722399100@yahoo.com.tw>
2025-07-06 22:55:49 +0800

    新增 Ctrl+C 後釋放 semaphore 的功能

--- src/main.c
+++ src/main.c
@@ -5,6 +5,7 @@
 #include <pthread.h>
 #include <netinet/in.h>
 #include "../include/job_handler.h"
+#include <signal.h>
 
 #define PORT 8888
 #define MAX_CLIENTS 10
@@ -13,6 +14,9 @@
 sem_t cargo_sem;
 sem_t qc_sem;
 
+int server_fd;  // 提升為全域變數，供 cleanup 使用
+
+// 客戶端處理程序
 void* client_handler(void* arg) {
     int* data = (int*)arg;
     int client_sock = data[0];
@@ -30,12 +34,22 @@ void* client_handler(void* arg) {
     return NULL;
 }
 
+// 當 Ctrl+C 時清理 semaphore 與 socket
+void cleanup(int signum) {
+    close(server_fd); // 關閉 socket
+    sem_destroy(&cargo_sem);
+    sem_destroy(&qc_sem);
+    printf("\n[Server] 資源已釋放，關閉服務\n");
+    exit(0);
+}
 
 int main() {
-    sem_init(&cargo_sem, 0, 3);  // 可同時處理 3 筆 cargo 類任務
+    signal(SIGINT, cleanup);
+
+    sem_init(&cargo_sem, 0, 3);  // 可同時處理 3 筆 cargo 任務
     sem_init(&qc_sem, 0, 1);     // 可同時處理 1 筆 QC 任務
 
-    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in addr = {0};
     addr.sin_family = AF_INET;
     addr.sin_port = htons(PORT);
@@ -55,8 +69,9 @@ int main() {
 
         pthread_t tid;
         pthread_create(&tid, NULL, client_handler, pclient);
-        pthread_detach(tid);
+        pthread_detach(tid);  // 自動釋放資源
     }
 
     return 0;
 }
+
